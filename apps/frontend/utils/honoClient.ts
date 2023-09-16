de2343433c0266e2a31e34b30cfedc68b06528ee
elyeser.randrianandraina <elyeser.randrianandraina@vooban.com>
2023-09-16 09:00:13 -0400

    build

--- apps/frontend/utils/honoClient.ts
+++ apps/frontend/utils/honoClient.ts
@@ -1,6 +1,6 @@
 import type { AppType } from "backend";
 import { hc } from "hono/client";
 
-const client = hc<AppType>(process.env.NEXT_PUBLIC_API_ENDPOINT);
+const client = hc<AppType>(process.env.NEXT_PUBLIC_API_ENDPOINT || "");
 
 export default client;
