7b97342f27138730a836d0281e974a544390432a
elyeser.randrianandraina <elyeser.randrianandraina@vooban.com>
2023-09-24 22:29:34 -0400

    styling

--- apps/frontend/src/app/songs/[id]/page.tsx
+++ apps/frontend/src/app/songs/[id]/page.tsx
@@ -13,11 +13,13 @@ export default async function Page({ params }: { params: { id: string } }) {
     <div>
       <Link href="/songs" className="flex items-center">
         <ArrowLeft size={25} strokeWidth={3} />
-        Back To Songs
+        Retour aux chants
       </Link>
       <main className="w-full h-full flex flex-col justify-center items-center ">
         <div>
-          <h2 className="text-3xl capitalize mb-10">{song.title}</h2>
+          <h2 className="text-3xl capitalize mb-10 text-center">
+            {song.title}
+          </h2>
           <pre>{song.lyrics}</pre>
         </div>
       </main>
